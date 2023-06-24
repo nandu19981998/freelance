d9a3835b8e39856a6c1a6b638f1414773bea7e92
khaleel_pv <khaleel@fairpe.com>
2023-06-24 17:43:32 +0530

    backend done

--- user-profile/pages/api/user/index.js
+++ user-profile/pages/api/user/index.js
@@ -1,29 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introductio
-const mongoose = require('mongoose');
-const UserDetails = require('../../../models/User')
 
-const uri = 'mongodb+srv://pvananthakrishnan17:mongodb123@cluster0.ojzhdqq.mongodb.net/freelance-userdetails?retryWrites=true&w=majority'
-
-mongoose.connect(uri, {
-  useUnifiedTopology: true,
-}) .then(() => {
-    console.log('Connected to MongoDB');
-    // Start executing your database operations here
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
-
-async function getAllUserDetails() {
-  try {
-    const documents = await UserDetails.find({});
-    console.log(documents);
-  } catch (error) {
-    console.error('Error retrieving documents:', error);
-  }
-}
-
-export default async function handler(req, res) {
-  getAllUserDetails();
-  res.status(200).json({name: "PV"});
+import clientPromise from "../../../models/mongo";
+export default async function handler(req, res){
+    const client = await clientPromise;
+    const db = client.db("freelance-demo");
+    try{
+      const allUsers = await db.collection("UserDetails").find({}).sort({_id:-1}).toArray();
+      res.json({ status: 200, data: allUsers });
+    }
+    catch(err) {
+        console.log(err)
+        res.json({message:"Something went wrong"});
+    }
 }
