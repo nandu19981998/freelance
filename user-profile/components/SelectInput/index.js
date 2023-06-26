b09634771017ad08767a980b0b51c5e1672c0248
ananthakrishnanpv1997 <137254138+ananthakrishnanpv1997@users.noreply.github.com>
2023-06-26 13:20:12 +0530

    fix

--- user-profile/components/SelectInput/index.js
+++ user-profile/components/SelectInput/index.js
@@ -6,8 +6,8 @@ const SelectInput = ({label,name, options, errorStatus, ...rest}) => {
     return <div className={styles.selectContainer}>
                 <label>{label}</label>
                 <select {...rest} name={name} className={styles.select}>
-                    <option value="">Select an option</option>
-                    {options.map((option) => <option value={option}>{option}</option>)}
+                    <option key="default" value="">Select an option</option>
+                    {options.map((option, index) => <option key={index+ option} value={option}>{option}</option>)}
                 </select>
                 {errorStatus.error && <span>{errorStatus.errorMessage}</span>}
           </div>
